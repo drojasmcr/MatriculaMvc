f4d385c0ebe6e28fd773294ac47f864c4458417d
Luis D. Rojas <diego@rojascr.net>
2020-10-09 11:32:35 -0600

    Agregar modelo de materia

--- Models/Materia.cs
+++ Models/Materia.cs
@@ -1,3 +1,10 @@
 namespace MvcMatricula.Models
 {
+    public class Materia 
+    {
+        public int Id {get;set;}
+        public string CodigoMateria {get;set;}
+        public int Creditos {get;set;}
+        public bool EstaBorrado {get;set;}
+    }
 }
