c348ea08140b8dc3c06a14ec22acbbaf345962a6
Luis D. Rojas <diego@rojascr.net>
2020-10-09 09:50:36 -0600

    Initial  commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcMatricula.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
