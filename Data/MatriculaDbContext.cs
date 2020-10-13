cdc6eeb46300e64e9330bed1ddee7d2391238a6b
Luis D. Rojas <diego@rojascr.net>
2020-10-12 18:53:59 -0600

    scripts de la base de datos en contenedores

--- Data/MatriculaDbContext.cs
+++ Data/MatriculaDbContext.cs
@@ -6,5 +6,7 @@ namespace MvcMatricula.Data
     public class MatriculaDbContext : DbContext
     {
         public DbSet<Materia> Materias {get;set;}
+
+        string connectionString = @"Data Source=127.0.0.1,1433;Database=dbmatriculal;User Id=SA;Password=Alajuela200*";
     }
 }
