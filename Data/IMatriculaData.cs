f6bbba04354e853299d4efafb71fe6b3176c9500
Luis D. Rojas <diego@rojascr.net>
2020-10-14 16:56:15 -0600

    Inrteface Matricula Datos

---
+++ Data/IMatriculaData.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using MvcMatricula.Models;
+
+public interface IMatriculaData 
+{
+    IEnumerable<Materia> ObtenerMaterias();
+    Materia ObtenerMateria( int id);
+    Materia AgregarMateria(Materia materia);
+    Materia ActualizarMateria(Materia materia);
+    Materia EliminarMateria( int id);
+    int Commit();
+
+}
